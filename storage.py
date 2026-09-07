e7be58a9b08636f9f9d3f80a378fe65054cfc493
Andrii Yevlakh <andriiyevlakh@gmail.com>
2026-09-07 16:21:59 +0200

    Small fix of storeg.py

--- storage.py
+++ storage.py
@@ -6,14 +6,14 @@ from pathlib import Path
 path = Path("passwords.json")
 
 def encode_to_json(data) -> None:
-    with open(path, "w") as file:
+    with path.open("w") as file:
         json.dump(data, file, indent=2)
         
 
 
 def decode_to_json() -> dict:
     if path.exists():
-        with open(path, "r") as file:
+        with path.open("r") as file:
             data = json.load(file)
             return data
     else:
